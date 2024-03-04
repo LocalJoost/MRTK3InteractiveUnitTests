15c79f109949c9908cb799944dea4ee02a3efbbb
Joost van Schaik <joostvanschaik@outlook.com>
2024-03-04 20:01:57 +0100

    Refactoring and enhancement

---
+++ Assets/InteractionTests/BaseRuntimeHandInputTests.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections;
+using MixedReality.Toolkit;
+using MixedReality.Toolkit.Core.Tests;
+using MixedReality.Toolkit.Input.Tests;
+using UnityEditor;
+using UnityEngine;
+using Object = UnityEngine.Object;
+
+namespace InteractionTests
+{ 
+    public abstract class BaseRuntimeHandInputTests : BaseRuntimeInputTests
+    {
+        protected const int HandMoveSteps = 1;
+        protected const int UpdateFrames = 1;
+        protected const float HandInFrontOfGameObject = 0.15f;
+        protected const float InitialHandInFrontOfUserDistance = 0.2f;
+        
+        protected GameObject InstantiatePrefab(string guid)
+        {
+            var prefabPath = AssetDatabase.GUIDToAssetPath(guid);
+            var prefab = AssetDatabase.LoadAssetAtPath(prefabPath, typeof(Object));
+            return Object.Instantiate(prefab) as GameObject;
+        }
+        
+        protected IEnumerator GetHand(Vector3 initialHandPosition, Action<TestHand> action)
+        {
+            var hand = new TestHand(Handedness.Right);
+            yield return hand.Show(initialHandPosition);
+            yield return RuntimeTestUtilities.WaitForUpdates(UpdateFrames);
+            action(hand);
+        }
+
+        protected Vector3 GetInitialHandPositionBefore(
+            GameObject testGameObject, 
+            float initialDistance = HandInFrontOfGameObject)
+        {
+            return testGameObject.transform.position - Vector3.forward * initialDistance;
+        }
+        
+        protected Vector3 GetInitialHandPosition(
+            float initialDistance = InitialHandInFrontOfUserDistance)
+        {
+            return InputTestUtilities.InFrontOfUser(Vector3.forward * initialDistance);
+        }
+
+        protected IEnumerator PokeHand(TestHand hand, float distance)
+        {
+            yield return MoveHand(hand, Vector3.forward * distance);
+            yield return MoveHand(hand, -Vector3.forward * distance);
+        }
+
+        protected IEnumerator MoveHand(TestHand hand, Vector3 distance)
+        {
+            yield return hand.Move(distance, HandMoveSteps);
+            yield return RuntimeTestUtilities.WaitForUpdates(UpdateFrames);
+        }
+        
+        protected IEnumerator MoveHandTo(TestHand hand, Vector3 location)
+        {
+            yield return hand.MoveTo(location, HandMoveSteps);
+            yield return RuntimeTestUtilities.WaitForUpdates(UpdateFrames);
+        }
+        
+        protected GameObject FindByName(GameObject parent, string name)
+        {
+            if (parent.name == name)
+            {
+                return parent;
+            }
+            foreach (Transform child in parent.transform)
+            {
+                var result = FindByName(child.gameObject, name);
+                if (result != null)
+                {
+                    return result;
+                }
+            }
+            return null;
+        }
+    }
+}
--- Assets/InteractionTests/ButtonsTests.cs
+++ Assets/InteractionTests/ButtonsTests.cs
@@ -1,158 +1,110 @@
-using System;
 using System.Collections;
 using System.Collections.Generic;
-using MixedReality.Toolkit;
-using MixedReality.Toolkit.Core.Tests;
 using MixedReality.Toolkit.Input.Tests;
 using MixedReality.Toolkit.UX;
 using NUnit.Framework;
-using UnityEditor;
 using UnityEngine;
 using UnityEngine.TestTools;
 using System.Linq;
 using UnityEngine.UI;
 using Object = UnityEngine.Object;
-using Vector3 = UnityEngine.Vector3;
 
-public class ButtonsTests : BaseRuntimeInputTests
+namespace InteractionTests
 {
-    private const string MenuGuid = "e9ddf3517c4b9c7488c12bdec6a9917f";
-    private const int HandMoveSteps = 1;
-    private const int UpdateFrames = 1;
-    private GameObject testGameObject;
-    private Vector3 initialTestGameObjectPosition;
-    private List<PressableButton> buttons;
+    public class ButtonsTests : BaseRuntimeHandInputTests
+    {
+        private const string MenuGuid = "e9ddf3517c4b9c7488c12bdec6a9917f";
+        private const string ButtonHolder = "Buttons-GridLayout";
+        private GameObject testGameObject;
+        private List<PressableButton> buttons;
 
-    [SetUp]
-    public void Init()
-    {
-        testGameObject = InstantiatePrefab(MenuGuid);
-        initialTestGameObjectPosition = testGameObject.transform.position;
-        var gridLayout = testGameObject.GetComponentInChildren<GridLayoutGroup>();
-        buttons = gridLayout.GetComponentsInChildren<PressableButton>().ToList();
-    }
- 
-    public override IEnumerator TearDown()
-    {
-        yield return base.TearDown();
-        Object.Destroy(testGameObject);
-    }
- 
-    [UnityTest]
-    public IEnumerator ButtonPressDoesNotEnableOtherButtons()
-    {
-        var initialHandPosition = GetInitialHandPosition();
-        var handDelta = initialTestGameObjectPosition.z - initialHandPosition.z;
-        TestHand hand = null;
-        yield return GetHand(initialHandPosition, h =>
+        [SetUp]
+        public void Init()
         {
-            hand = h;
-        });
-        Assert.AreEqual(0, GetToggledButtonCount());
-        yield return PokeHand(hand, handDelta);
-        Assert.AreEqual(1, GetToggledButtonCount());
-        yield return PokeHand(hand, handDelta);
-        Assert.AreEqual(0, GetToggledButtonCount());
-    }
-    
-    [UnityTest]
-    public IEnumerator PressingTwoDifferentButtonsShouldOnlySelectTheLast()
-    {
-        var pressedButtons = new List<PressableButton>();
-        var initialHandPosition = GetInitialHandPosition();
-        var handDelta = initialTestGameObjectPosition.z - initialHandPosition.z;
-        TestHand hand = null;
-        yield return GetHand(initialHandPosition, h =>
-        {
-            hand = h;
-        });
-        
-        Assert.AreEqual(0, GetToggledButtonCount());
-        
-        yield return PokeHand(hand, handDelta);
-        Assert.AreEqual(1, GetToggledButtonCount());
-        AddButtonToPressedList(pressedButtons);
-        
-        yield return MoveHand(hand, -Vector3.right * 0.04f);
-        yield return PokeHand(hand, handDelta);
-        Assert.AreEqual(1, GetToggledButtonCount());
-        AddButtonToPressedList(pressedButtons);
-        
-        yield return MoveHand(hand, -Vector3.up * 0.04f);
-        yield return PokeHand(hand, handDelta);
-        Assert.AreEqual(1, GetToggledButtonCount());
-        AddButtonToPressedList(pressedButtons);
-        
-        yield return MoveHand(hand, Vector3.right * 0.04f);
-        yield return PokeHand(hand, handDelta);
-        Assert.AreEqual(1, GetToggledButtonCount());
-        AddButtonToPressedList(pressedButtons);
-    }
-
-    [UnityTest]
-    public IEnumerator PressingCloseCloseMenu()
-    {
-        Assert.IsFalse(testGameObject == null);
-        var initialHandPosition = GetInitialHandPosition() + Vector3.up * 0.02f;
-        var handDelta = initialTestGameObjectPosition.z - initialHandPosition.z;
-        TestHand hand = null;
-        yield return GetHand(initialHandPosition, h =>
-        {
-            hand = h;
-        });
-        yield return hand.Show(initialHandPosition);
-        yield return RuntimeTestUtilities.WaitForUpdates(UpdateFrames);
-        yield return PokeHand(hand, handDelta);
-        Assert.IsTrue(testGameObject == null);
-    }
-    
-    private IEnumerator GetHand(Vector3 initialHandPosition, Action<TestHand>action)
-    {
-        var hand = new TestHand(Handedness.Right);
-        yield return hand.Show(initialHandPosition);
-        yield return RuntimeTestUtilities.WaitForUpdates(UpdateFrames);
-        action(hand);
-    }
-    
-    private Vector3 GetInitialHandPosition()
-    {
-        return InputTestUtilities.InFrontOfUser(0.3f) + Vector3.up * 0.015f + Vector3.right * 0.005f;
-    }
-
-    private IEnumerator PokeHand(TestHand hand, float handDelta)
-    {
-        yield return MoveHand(hand, Vector3.forward * handDelta);
-        yield return MoveHand(hand, -Vector3.forward * handDelta );
-    }
-
-    private IEnumerator MoveHand(TestHand hand, Vector3 handDelta)
-    {
-        yield return hand.Move(handDelta, HandMoveSteps);
-        yield return RuntimeTestUtilities.WaitForUpdates(UpdateFrames);
-    }
-
-    private int GetToggledButtonCount()
-    {
-        return buttons.Count(b=> b.IsToggled);
-    }
-    
-    private void AddButtonToPressedList(List<PressableButton> pressedButtons)
-    {
-        var button = buttons.FirstOrDefault(b=> b.IsToggled);
-        if (!pressedButtons.Contains(button))
-        {
-            pressedButtons.Add(button);
+            testGameObject = InstantiatePrefab(MenuGuid);
+            var gridLayout = testGameObject.GetComponentInChildren<GridLayoutGroup>();
+            buttons = gridLayout.GetComponentsInChildren<PressableButton>().ToList();
         }
-        else
+
+        public override IEnumerator TearDown()
         {
-            Assert.Fail("Button already pressed");
+            yield return base.TearDown();
+            Object.Destroy(testGameObject);
         }
-    }
-    
-    private GameObject InstantiatePrefab(string guid)
-    {
-        var prefabPath = AssetDatabase.GUIDToAssetPath(guid);
-        var prefab = AssetDatabase.LoadAssetAtPath(prefabPath, typeof(Object));
-        return Object.Instantiate(prefab) as GameObject;
+
+        [UnityTest]
+        public IEnumerator ButtonPressDoesNotEnableOtherButtons()
+        {
+            var button = 
+                FindByName(testGameObject, ButtonHolder)
+                    .GetComponentInChildren<PressableButton>();
+            var initialHandPosition =
+                GetInitialHandPositionBefore(button.gameObject, HandInFrontOfGameObject);
+            TestHand hand = null;
+            yield return GetHand(initialHandPosition, h => { hand = h; });
+            Assert.AreEqual(0, GetToggledButtonCount());
+            yield return PokeHand(hand, HandInFrontOfGameObject);
+            Assert.AreEqual(1, GetToggledButtonCount());
+            yield return PokeHand(hand, HandInFrontOfGameObject);
+            Assert.AreEqual(0, GetToggledButtonCount());
+        }
+
+        [UnityTest]
+        public IEnumerator PressingTwoDifferentButtonsShouldOnlySelectTheLast()
+        {
+            var pressedButtons = new List<PressableButton>();
+            var availableButtons = 
+                FindByName(testGameObject, ButtonHolder).
+                    GetComponentsInChildren<PressableButton>();
+            var initialHandPosition = GetInitialHandPosition();
+            TestHand hand = null;
+            yield return GetHand(initialHandPosition, h => { hand = h; });
+            Assert.AreEqual(0, GetToggledButtonCount());
+            
+            foreach(var button in availableButtons)
+            {
+                var handPosition = 
+                    GetInitialHandPositionBefore(button.gameObject, HandInFrontOfGameObject);
+                yield return MoveHandTo(hand, handPosition);
+                yield return PokeHand(hand, HandInFrontOfGameObject);
+                Assert.AreEqual(1, GetToggledButtonCount());
+                AddButtonToPressedList(pressedButtons);
+            }
+            Assert.AreEqual(availableButtons.Length, pressedButtons.Count);
+        }
+
+        [UnityTest]
+        public IEnumerator PressingCloseCloseMenu()
+        {
+            Assert.IsFalse(testGameObject == null);
+            var button = 
+                FindByName(testGameObject, "Close");
+            var initialHandPosition =
+                GetInitialHandPositionBefore(button.gameObject,
+                    HandInFrontOfGameObject);
+            TestHand hand = null;
+            yield return GetHand(initialHandPosition, h => { hand = h; });
+            
+            yield return PokeHand(hand, HandInFrontOfGameObject);
+            Assert.IsTrue(testGameObject == null);
+        }
+
+        private int GetToggledButtonCount()
+        {
+            return buttons.Count(b => b.IsToggled);
+        }
+
+        private void AddButtonToPressedList(List<PressableButton> pressedButtons)
+        {
+            var button = buttons.FirstOrDefault(b => b.IsToggled);
+            if (!pressedButtons.Contains(button))
+            {
+                pressedButtons.Add(button);
+            }
+            else
+            {
+                Assert.Fail("Button already pressed");
+            }
+        }
     }
 }
